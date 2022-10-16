8b48fa3ddb14240c6a7483f6b344ced07c1263d4
Deepak <deepak.onlinework13@gmail.com>
2022-10-16 22:59:31 +0530

    baseURL changed

--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,8 @@ import { Provider } from "react-redux";
 import { store } from './Redux/store.js'
 import  axios  from "axios";
 
-axios.defaults.baseURL=`${process.env.REACT_APP_API_URL}`;
+// axios.defaults.baseURL=`${process.env.REACT_APP_API_URL}`;
+axios.defaults.baseURL= "https://mighty-wildwood-65043.herokuapp.com";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
